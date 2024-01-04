d3619a2997702acb039db99ed69151fc460e75f6
PranavRaghu <kingprawn5000@gmail.com>
2024-01-04 17:20:31 +0530

    Q3 Done

--- Q3/ImageConcatenation.sh
+++ Q3/ImageConcatenation.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-input_image="DeepSea.jpg"      # Replace with your input image
+input_image="Dumpyard.jpg"      # Replace with your input image
 zip_file="1000.zip"           # Replace with your ZIP file
-output_image="DeepDiveDiscovery.jpg"   # Replace with your output image
+output_image="FinalDump.jpg"   # Replace with your output image
 
 # Concatenate the image and ZIP file
 cat "$input_image" "$zip_file" > "$output_image"
